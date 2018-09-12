69047e77e30affd872540bcf9830a3085811283b
yubei <harrse@qq.com>
2018-09-12 16:47:05 +0800

    revised problems with swagger

--- test/simple/input/simpleDoc.js
+++ test/simple/input/simpleDoc.js
@@ -1,10 +1,14 @@
 /**
- * @api {get} /user/id Request User information
- * @apiName GetUser
- * @apiGroup User
- *
- * @apiParam {Number} id Users unique ID.
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
+ * @api {post} /api/admin/Login 登录
+ * @apiGroup Admin
+ * @apiParam {String} Name 账号
+ * @apiParam {String} Pass 密码
+ * @apiSuccess {Number} Status 状态码
+ * @apiSuccess {String} Message 消息
+ * @apiSuccess {Object} Data 略
+ * @apiSuccess {Number} Data.ID 账号ID
+ * @apiSuccess {String} Data.Nickname 昵称
+ * @apiSuccess {Object} Data.Role 角色信息
+ * @apiSuccess {String} Data.Role.Name 名称
+ * @apiSuccess {String} Data.Role.Aliase 别名
  */
